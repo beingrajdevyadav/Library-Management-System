76e3003273279e471311bb0e5ec3d9babb2ea4dc
Rajdev Yadav <myself.rajdev2024@gmail.com>
2024-12-25 04:08:00 +0530

    Functionality Added.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// -------------------------------------------------------
 // class to display the current date and time in the browser
-
+// ---------------------------------------------------------
 class PrintDateTime{
     constructor(dateTag, timeTag){
         this.dateElement = document.querySelector("#dateTag");
@@ -23,3 +24,120 @@ class PrintDateTime{
 
 let printDateTime = new PrintDateTime("#dateTag", "#timeTag");
 printDateTime.start();
+
+// ----------------------------------------------
+//  student class 
+// ----------------------------------------------
+
+class Student{
+    constructor(name){
+        this.name = name;
+    }
+}
+
+// ----------------------------------------------
+// book class
+// ----------------------------------------------
+
+class Book{
+    constructor(title){
+        this.title = title;
+    }
+}
+
+// ----------------------------------------------
+// transaction class
+// ----------------------------------------------
+
+class Transaction{
+    constructor(student, book, type, date, time){
+        this.student = student;
+        this.book = book;
+        this.type = type;
+        this.date = date;
+        this.time = time;
+    }
+}
+
+// ----------------------------------------------
+// date time class
+// ----------------------------------------------
+class DateTime{
+    constructor(){
+        this.date = new Date();
+    }
+
+    getFormattedDate(){
+        return this.date.toLocaleDateString();
+    }
+    getFormattedTime(){
+        return this.date.toLocaleTimeString();
+    }
+}
+
+// ----------------------------------------------
+// function to add transaction
+// ----------------------------------------------
+
+function addTransaction(){
+   // html elements selection
+    const studentName = document.querySelector("#studentName").value;
+    const bookTitle = document.querySelector("#bookTitle").value;
+    const transactionType = document.querySelector("#transactionType").value;
+
+    // check if the input fields are not empty
+    if(studentName && bookTitle && transactionType){
+        const student = new Student(studentName);
+        const book = new Book(bookTitle);
+        const dateTime = new DateTime();
+        const transaction = new Transaction(student, book, transactionType, dateTime.getFormattedDate(), dateTime.getFormattedTime());
+    
+        console.log(transaction);
+    
+        let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+    
+        transactions.push(transaction);
+    
+        localStorage.setItem("transactions", JSON.stringify(transactions));
+
+        displayTransactions();
+    }
+    
+}
+
+// ----------------------------------------------
+// function to display transactions
+// ----------------------------------------------
+
+function displayTransactions(){
+    const transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+    const transactionTableBody = document.querySelector("#transactionTableBody");
+    transactionTableBody.innerHTML = "";
+
+    transactions.forEach((transaction, index)=>{
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <td>${index + 1}</td>
+        <td>${transaction.student.name}</td>
+        <td>${transaction.book.title}</td>
+        <td>${transaction.type}</td>
+        <td>${transaction.date}</td>
+        <td>${transaction.time}</td>
+        `;
+        transactionTableBody.appendChild(tr);
+    });
+}
+
+// ----------------------------------------------
+// function to clear transactions
+// ----------------------------------------------
+function clearTransactions(){
+    localStorage.clear();
+    displayTransactions();
+}
+
+// ----------------------------------------------
+// event listeners for add transaction and clear transaction
+// ----------------------------------------------
+window.onload = displayTransactions();
